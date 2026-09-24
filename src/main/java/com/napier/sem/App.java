98d559662b9801db0c055f0405dee877c2bc9ae1
hlumhtetaung <hlumhtetaung121@gmail.com>
2026-09-24 16:23:58 +0630

    lab03a done

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -77,6 +77,15 @@ public class App
         // Connect to database
         a.connect();
 
+        // Get Employee details
+        Employee emp = a.getEmployee(255530);
+
+        // Print employee details to console
+        if (emp != null)
+        {
+            System.out.println(emp.emp_no + " " + emp.first_name + " " + emp.last_name);
+        }
+
         // Disconnect from database
         a.disconnect();
     }
@@ -112,6 +121,21 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
+        }
+    }
+
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 }
--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -4,13 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // Create new Application
-        App a = new App();
-
-        // Connect to database
-        a.connect();
-
-        // Disconnect from database
-        a.disconnect();
+        System.out.print("Main class");
     }
 }
